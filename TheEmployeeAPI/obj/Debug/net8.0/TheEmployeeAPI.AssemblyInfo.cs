6987ab70a8b36adbe614d277c2c3a7982a121ab1
Steve Shinn <sqs5468@gmail.com>
2026-03-12 17:10:32 -0400

    not sure - add to gitignore

--- TheEmployeeAPI/obj/Debug/net8.0/TheEmployeeAPI.AssemblyInfo.cs
+++ TheEmployeeAPI/obj/Debug/net8.0/TheEmployeeAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TheEmployeeAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+521dc52b5385ff82772a87f6719541553618ec71")]
 [assembly: System.Reflection.AssemblyProductAttribute("TheEmployeeAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TheEmployeeAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
